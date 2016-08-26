3bfc7bb3f85e1a943b75cede59d28a94e8fb8f4a
kevin <kevin@DESKTOP-C3GPI5E>
2016-08-26 16:09:52 -0500

    Tercer Envio

--- TareING/src/tareing/TareING.java
+++ TareING/src/tareing/TareING.java
@@ -6,22 +6,29 @@
 
 package tareing;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kevin
  */
 public class TareING {
     
+    Scanner entrada = new Scanner(System.in);
+    int ej = Integer.parseInt(entrada.nextLine());  
     int trabajos=0, aporte=0, examen=0;
     int notaPUno = 0;
     int notaPDos = 0;
     int promedioUno = 0, PromedioDos = 0;
-    /**
-     * @param args the command line arguments
-     */
+   
+    public void opciones (){
+        System.out.println("Ingrese nota de trabajos: ");
+        Integer.parseInt(entrada.nextLine());
+    }
+    
     public static void main(String[] args) {
-        
-        
+        TareING opciones = new TareING();
+        opciones.opciones();
     }
     
 }
