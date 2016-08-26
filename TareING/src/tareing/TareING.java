4739613ad2e056b47dd51dca3fdb31cdcb2be08f
kevin <kevin@DESKTOP-C3GPI5E>
2016-08-26 15:50:42 -0500

    Segundo Envio

--- TareING/src/tareing/TareING.java
+++ TareING/src/tareing/TareING.java
@@ -12,7 +12,10 @@ package tareing;
  */
 public class TareING {
     
-
+    int trabajos=0, aporte=0, examen=0;
+    int notaPUno = 0;
+    int notaPDos = 0;
+    int promedioUno = 0, PromedioDos = 0;
     /**
      * @param args the command line arguments
      */
